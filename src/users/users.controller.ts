0d90c1803d5bb637ca29bd3d354ea4dcf8466f52
Radostev Dmitry <dmitrii.radostev@domru.ru>
2021-11-20 17:43:33 +0500

    Выдача ролей и бан пользователей

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,10 @@
 import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -20,9 +24,28 @@ export class UsersController {
 
   @ApiOperation({summary: 'Получение всех пользователей'})
   @ApiResponse({status:200, type: [User]})
-  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
   getAll(){
     return this.userService.getAllUsers()
+  }
+
+  @ApiOperation({summary: 'Выдать роль'})
+  @ApiResponse({status:200})
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() dto: AddRoleDto){
+    return this.userService.addRole(dto)
+  }
+
+  @ApiOperation({summary: 'Забанить пользователя'})
+  @ApiResponse({status:200})
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('/ban')
+  banUser(@Body() dto: BanUserDto){
+    return this.userService.banUser(dto)
   }
 }
