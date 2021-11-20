752aaa286351d314b5f3310bed591f8d92f1dc28
Radostev Dmitry <dmitrii.radostev@domru.ru>
2021-11-20 15:55:37 +0500

    регистрация и авторизация по jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -19,6 +20,7 @@ export class UsersController {
 
   @ApiOperation({summary: 'Получение всех пользователей'})
   @ApiResponse({status:200, type: [User]})
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll(){
     return this.userService.getAllUsers()
